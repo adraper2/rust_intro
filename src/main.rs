dbd0dac04a5658a0ad8ac8389be140abfaaeb28d
adraper2 <aidan.draper@wework.com>
2022-12-31 15:32:50 -0500

    setting up project as CLI tool for creating website content

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 // this is a rust comment
 
 // how to use packages
+use clap::Parser;
 use std::io;
 use rand::Rng;
+use serde_json::{Result, Value};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 
 fn main() {
+    // get user input at command line
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+    
+    let args = Cli {
+    pattern: pattern,
+    path: std::path::PathBuf::from(path),
+    };
+
     let _print_times = rand::thread_rng().gen_range(1..5);
     println!("Hello, world! Write something below:");
     
@@ -25,3 +43,5 @@ fn main() {
 
     }
 }
+
+fn append_new_content() -> Result<()> 
